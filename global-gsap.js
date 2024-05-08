d85eab12b3fc75b34d8076f3654abf55cbb5ff9e
Dmitry <kapakulii@gmail.com>
2024-05-08 13:14:55 +0300

    240508

---
+++ global-gsap.js
@@ -0,0 +1,24 @@
+// GSAP animation
+const duration = 1;
+const ease = "power3.inOut";
+
+gsap.defaults({
+    ease: ease,
+    duration: duration
+});
+
+// Global animation
+const fadeOn = document.querySelectorAll('[fade-on]');
+fadeOn.forEach(
+    container => {
+        gsap.from(container, {
+            y: '1rem',
+            scale: 0.98,
+            autoAlpha: 0,
+            scrollTrigger: {
+                trigger: container,
+                start: 'top 95%',
+                toggleActions: 'play none none reverse'
+            }
+        });
+    });
