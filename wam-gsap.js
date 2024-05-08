546bb3dab7f5e270ff628c57b48b6a47e521d406
Dmitry <kapakulii@gmail.com>
2024-05-08 14:27:15 +0300

    240508

--- wam-gsap.js
+++ wam-gsap.js
@@ -1,4 +1,34 @@
 // GSAP animation
+const duration = 1;
+const ease = "power3.inOut";
+
+gsap.defaults({
+  ease: ease,
+  duration: duration
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger)
+});
+
+// Global animation
+const fadeOn = document.querySelectorAll('[fade-on]');
+
+fadeOn.forEach(
+  container => {
+    gsap.from(container, {
+      y: '1rem',
+      scale: 0.98,
+      autoAlpha: 0,
+      scrollTrigger: {
+        trigger: container,
+        start: 'top 95%',
+        toggleActions: 'play none none reverse'
+      }
+    });
+  });
+
+// GSAP animation
 // Global animation
 let mediaScreen = gsap.matchMedia();
 
