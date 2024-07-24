1eb606381c68e5aac3bee609ed63477382250bae
Dmitry <kapakulii@gmail.com>
2024-07-24 16:50:46 +0300

    240724

--- global-gsap.js
+++ global-gsap.js
@@ -1,29 +1,29 @@
-// // GSAP animation
-// const duration = 1;
-// const ease = "power3.inOut";
+// GSAP animation
+const duration = 1;
+const ease = "power3.inOut";
 
-// gsap.defaults({
-//     ease: ease,
-//     duration: duration
-// });
+gsap.defaults({
+    ease: ease,
+    duration: duration
+});
 
-// document.addEventListener("DOMContentLoaded", (event) => {
-//     gsap.registerPlugin(ScrollTrigger)
-// });
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(ScrollTrigger)
+});
 
-// // Global animation
-// const fadeOn = document.querySelectorAll('[fade-on]');
+// Global animation
+const fadeOn = document.querySelectorAll('[fade-on]');
 
-// fadeOn.forEach(
-//     container => {
-//         gsap.from(container, {
-//             y: '1rem',
-//             scale: 0.98,
-//             autoAlpha: 0,
-//             scrollTrigger: {
-//                 trigger: container,
-//                 start: 'top 95%',
-//                 toggleActions: 'play none none reverse'
-//             }
-//         });
-//     });
+fadeOn.forEach(
+    container => {
+        gsap.from(container, {
+            y: '1rem',
+            scale: 0.98,
+            autoAlpha: 0,
+            scrollTrigger: {
+                trigger: container,
+                start: 'top 95%',
+                toggleActions: 'play none none reverse'
+            }
+        });
+    });
