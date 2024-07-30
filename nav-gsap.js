7188f5123366a8bf40131721a2c8bef9e44842e0
Dmitry <kapakulii@gmail.com>
2024-07-30 07:44:22 +0300

    240730

--- nav-gsap.js
+++ nav-gsap.js
@@ -2,79 +2,86 @@
 const triggerElement = document.querySelector('[dropdown-calendly="trigger"]');
 const contentElement = document.querySelector('[dropdown-calendly="container"]');
 
-gsap.set(contentElement, {
-    autoAlpha: 0,
-    display: 'flex',
-    y: '-3rem',
-    x: '2rem',
-    scale: 0.8
-});
+// Проверяем, существуют ли элементы на странице
+if (triggerElement && contentElement) {
 
-let isAnimating = false;
-let timer = null;
-const delay = 2000; // 2 секунды в миллисекундах
+    gsap.set(contentElement, {
+        autoAlpha: 0,
+        display: 'flex',
+        y: '-3rem',
+        x: '2rem',
+        scale: 0.8
+    });
 
-const startHideTimer = () => {
-    if (timer) {
-        clearTimeout(timer);
-    }
-    timer = setTimeout(() => {
+    let isAnimating = false;
+    let timer = null;
+    const delay = 2000; // 2 секунды в миллисекундах
+
+    const startHideTimer = () => {
+        if (timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+            if (!isAnimating) {
+                hideContentElement();
+            }
+        }, delay);
+    };
+
+    const showContentElement = () => {
+        gsap.to(contentElement, {
+            autoAlpha: 1,
+            duration: 0.5,
+            y: '0rem',
+            x: '0rem',
+            scale: 1,
+            ease: "elastic.out(1, 0.5)"
+        });
+    };
+
+    const hideContentElement = () => {
+        isAnimating = true;
+        gsap.to(contentElement, {
+            autoAlpha: 0,
+            duration: 0.5,
+            ease: "power2.inOut",
+            onComplete: () => {
+                gsap.set(contentElement, {
+                    y: '-3rem',
+                    x: '2rem',
+                    scale: 0.8,
+                });
+                isAnimating = false;
+            }
+        });
+    };
+
+    triggerElement.addEventListener('mouseenter', () => {
         if (!isAnimating) {
+            showContentElement();
+        }
+        if (timer) {
+            clearTimeout(timer);
+        }
+    });
+
+    triggerElement.addEventListener('mouseleave', startHideTimer);
+    contentElement.addEventListener('mouseleave', startHideTimer);
+
+    document.addEventListener('click', (event) => {
+        if (!contentElement.contains(event.target) && !triggerElement.contains(event.target) && !
+            isAnimating) {
             hideContentElement();
         }
-    }, delay);
-};
-
-const showContentElement = () => {
-    gsap.to(contentElement, {
-        autoAlpha: 1,
-        duration: 0.5,
-        y: '0rem',
-        x: '0rem',
-        scale: 1,
-        ease: "elastic.out(1, 0.5)"
     });
-};
 
-const hideContentElement = () => {
-    isAnimating = true;
-    gsap.to(contentElement, {
-        autoAlpha: 0,
-        duration: 0.5,
-        ease: "power2.inOut",
-        onComplete: () => {
-            gsap.set(contentElement, {
-                y: '-3rem',
-                x: '2rem',
-                scale: 0.8,
-            });
-            isAnimating = false;
-        }
+    contentElement.addEventListener('click', (event) => {
+        event.stopPropagation();
     });
-};
 
-triggerElement.addEventListener('mouseenter', () => {
-    if (!isAnimating) {
-        showContentElement();
-    }
-    if (timer) {
-        clearTimeout(timer);
-    }
-});
-
-triggerElement.addEventListener('mouseleave', startHideTimer);
-contentElement.addEventListener('mouseleave', startHideTimer);
-
-document.addEventListener('click', (event) => {
-    if (!contentElement.contains(event.target) && !triggerElement.contains(event.target) && !
-        isAnimating) {
-        hideContentElement();
-    }
-});
-
-contentElement.addEventListener('click', (event) => {
-    event.stopPropagation();
-});
+} else {
+    console.log("Calendo animation not found.");
+}
 
 // Отключаем скролл, если мобильное меню открыто
 const burger = document.querySelector('.header_burger-wrapper');
