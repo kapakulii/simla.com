490302e4b9a57ede453f473866dedc58158293fb
Dmitry <kapakulii@gmail.com>
2024-07-31 14:30:59 +0300

    240731

--- gsap/crm_gsap.js
+++ gsap/crm_gsap.js
@@ -1,17 +1,22 @@
+let mediaScreen = gsap.matchMedia();
 var cardFeatures = document.querySelectorAll('[crm-size-reduce]');
 
-// Получаем общее количество элементов
-var totalElements = cardFeatures.length;
+mediaScreen.add("(min-width: 768px)", (context) => {
 
-gsap.utils.toArray(cardFeatures).forEach(function (element, index) {
-    // Вычисляем задержку на основе индекса элемента
-    var delay = index * 0.5; // Например, задержка увеличивается на 0.5 секунды для каждого следующего элемента
+    // Получаем общее количество элементов
+    var totalElements = cardFeatures.length;
 
-    gsap.to(element, {
-        scale: 0.95,
-        yoyo: true,
-        repeat: -1,
-        duration: 2,
-        delay: delay // Применяем задержку к анимации
+    gsap.utils.toArray(cardFeatures).forEach(function (element, index) {
+        // Вычисляем задержку на основе индекса элемента
+        var delay = index * 0.5; // Например, задержка увеличивается на 0.5 секунды для каждого следующего элемента
+
+        gsap.to(element, {
+            scale: 0.95,
+            yoyo: true,
+            repeat: -1,
+            duration: 2,
+            delay: delay // Применяем задержку к анимации
+        });
     });
-});
+
+})
