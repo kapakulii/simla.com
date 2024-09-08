1ee4c2044878c65c13a28fe65cefce0734acd0fa
Dmitry <kapakulii@gmail.com>
2024-09-08 11:40:00 +0300

    240908

--- text-fade-in-gsap.js
+++ text-fade-in-gsap.js
@@ -1,34 +1,30 @@
 let typeSplit = new SplitType('[intro-text]', {
     types: 'words',
     tagName: 'span'
-})
+});
 
-// SplitText animation Type 1
-gsap.from('[intro-text="late"] .word', {
-    opacity: 0.1,
-    duration: 3,
-    ease: 'power4.inOut',
-    stagger: 0.25,
+// Функция для создания анимации
+function createScrollAnimation(attr, startPos, endPos) {
+    const element = document.querySelector(`[intro-text="${attr}"]`);
 
-    scrollTrigger: {
-        trigger: '[intro-text="late"]',
-        start: 'top 70% center',
-        end: 'bottom 30% center',
-        scrub: 4
+    if (element) {
+        gsap.from(`[intro-text="${attr}"] .word`, {
+            opacity: 0.1,
+            duration: 3,
+            ease: 'power4.inOut',
+            stagger: 0.25,
+            scrollTrigger: {
+                trigger: element,
+                start: startPos,
+                end: endPos,
+                scrub: 4
+            }
+        });
     }
-})
+}
 
-// SplitText animation Type 2
-gsap.from('[intro-text="early"] .word', {
-    opacity: 0.1,
-    duration: 3,
-    ease: 'power4.inOut',
-    stagger: 0.25,
-
-    scrollTrigger: {
-        trigger: '[intro-text="early"]',
-        start: 'top 90% center',
-        end: 'bottom 60% center',
-        scrub: 4
-    }
-})
+// Запуск анимаций для разных элементов
+//Запустить позднее (Mobile)
+createScrollAnimation('late', 'top 70% center', 'bottom 30% center');
+//Запустить раньше (Home)
+createScrollAnimation('early', 'top 90% center', 'bottom 60% center');
