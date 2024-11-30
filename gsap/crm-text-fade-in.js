f77e3f0d238de21fe4bd140b5dc055defa13abc3
Dmitry <kapakulii@gmail.com>
2024-11-30 15:12:51 +0300

    241130

---
+++ gsap/crm-text-fade-in.js
@@ -0,0 +1,27 @@
+let typeSplit = new SplitType('[crm-intro-text]', {
+    types: 'words',
+    tagName: 'span'
+});
+
+const element = document.querySelectorAll(`[crm-intro-text]`);
+
+gsap.set(element, {
+    opacity: 1,
+});
+
+gsap.fromTo(`[crm-intro-text] .word`, {
+    opacity: 0.1,
+},
+    {
+        opacity: 1,
+        duration: 3,
+        ease: 'power4.inOut',
+        stagger: 0.25,
+        scrollTrigger: {
+            trigger: '.section_cwa-hero',
+            start: 'top top',
+            end: 'bottom center',
+            scrub: 10,
+            //markers: true
+        }
+    });
