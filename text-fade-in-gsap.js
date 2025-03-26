c0f153d8afa72b4e9c2134f48892ee701afe6589
Dmitry <kapakulii@gmail.com>
2025-03-26 14:19:21 +0300

    250326

--- text-fade-in-gsap.js
+++ text-fade-in-gsap.js
@@ -8,22 +8,21 @@ function createScrollAnimation(attr, startPos, endPos) {
     const elements = document.querySelectorAll(`[intro-text="${attr}"]`);
 
     if (elements.length > 0) {
-        gsap.fromTo(
-            `[intro-text="${attr}"] .word`,
-            { opacity: 0.1 }, // Начальное состояние
-            {
-                opacity: 1, // Конечное состояние
-                duration: 3,
-                ease: 'power4.inOut',
-                stagger: 0.25,
-                scrollTrigger: {
-                    trigger: elements[0], // Берём первый найденный элемент
-                    start: startPos,
-                    end: endPos,
-                    scrub: 4
-                }
+        // Устанавливаем начальное состояние только для нужных слов
+        gsap.set(`[intro-text="${attr}"] .word`, { opacity: 0.1 });
+
+        gsap.to(`[intro-text="${attr}"] .word`, {
+            opacity: 1, // Конечное состояние
+            duration: 3,
+            ease: 'power4.inOut',
+            stagger: 0.25,
+            scrollTrigger: {
+                trigger: elements[0], // Берём первый найденный элемент
+                start: startPos,
+                end: endPos,
+                scrub: 4
             }
-        );
+        });
     }
 }
 
